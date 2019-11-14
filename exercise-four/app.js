fec51d1a63b37ee9011adc27448415f713c4fd75
Oliwia <os813@nyu.edu>
2019-11-14 13:44:06 -0500

    homework

---
+++ exercise-four/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+const indexRoute = require("./routes/index.js");
+const aboutRoute = require("./routes/about.js");
+
+app.use("/", indexRoute);
+app.use("/about", aboutRoute);
+
+app.use("/static", express.static("public"));
+
+app.listen(port, () => `Example app listening on port ${port}!`);
