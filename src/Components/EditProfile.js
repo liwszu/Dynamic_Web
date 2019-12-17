11f7beb913dcf313c4dd3eaca3d5ddc4cb4bab17
Oliwia <os813@nyu.edu>
2019-12-17 10:51:35 -0500

    testing

--- src/Components/EditProfile.js
+++ src/Components/EditProfile.js
@@ -1,26 +1,29 @@
-import React from 'react'
+// import React from 'react'
 
-class NameForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: ''};
+// class NameForm extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {value: ''};
   
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
+//       this.handleChange = this.handleChange.bind(this);
+//       this.handleSubmit = this.handleSubmit.bind(this);
+//     }
   
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
+//     handleChange(event) {
+//       this.setState({value: event.target.value});
+//     }
   
-    handleSubmit(event) {
-      alert( this.state.value);
-      event.preventDefault();
-    }
+//     handleSubmit(event) {
+//       alert( this.state.value);
+//       event.preventDefault();
+//     }
   
-    render() {
+//     render() {
+import React from 'react'
+export default function EditProfile({customize}){
       return (
-        <form onSubmit={this.handleSubmit}>
+        <div>
+        <form onSubmit={e => customize(e)}>
           <label>
             Favorite Travel Destination:
             <input type="text" value={this.state.value} onChange={this.handleChange} />
@@ -35,6 +38,7 @@ class NameForm extends React.Component {
           </label>
           <input type="submit" value="Submit" />
         </form>
-      );
+        </div>
+      )
     }
-  }
+  
